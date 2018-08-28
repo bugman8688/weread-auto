60d47f0629f24d70a13b74ec99b648a5372bc882
hanb <hanb@heletech.com.cn>
2018-08-28 11:06:16 +0800

    1. 使用py3 2. 强迫症修改命名规则 3. 每过30分钟提醒心急的自己可以领币了

--- android.py
+++ android.py
@@ -1,30 +1,36 @@
-#coding:utf-8
+# coding:utf-8
 from uiautomator import device as d
 import time
 import datetime
-#点亮屏幕
-def lightScreen():
+
+
+# 点亮屏幕
+def light_screen():
     d.screen.on()
 
-#滑动页面
-def autoSwipe():
+
+# 滑动页面
+def auto_swipe():
     d.swipe(1000, 500, 200, 500)
     time.sleep(30)
+
 
 # 执行5小时
 if __name__ == '__main__':
     # 获取当前时间
-    startTime = datetime.datetime.now();
+    startTime = datetime.datetime.now()
     while 1:
-        nowTime = datetime.datetime.now();
-        mkt_last = time.mktime(startTime.timetuple());
-        mkt_now = time.mktime(nowTime.timetuple());
-        delt_time = (mkt_now-mkt_last)/60   #转成分钟
+        nowTime = datetime.datetime.now()
+        mkt_last = time.mktime(startTime.timetuple())
+        mkt_now = time.mktime(nowTime.timetuple())
+        delt_time = (mkt_now - mkt_last) / 60  # 转成分钟
         # 5小时 ＝＝＝ 300分钟
-        leftTime = 300 - delt_time ;
-        if leftTime > 0 :
-            print "剩余" + str(int(leftTime)) + '分钟';
-            autoSwipe();
+        leftTime = 300 - delt_time
+        if leftTime > 0:
+            if leftTime % 30 == 0 and leftTime != 300:
+                print("可以领一个书币了")
+            print("剩余" + str(int(leftTime)) + '分钟')
+            auto_swipe()
         else:
-            print "自动读书完毕";
-            break;
+            print("自动读书完毕")
+            break
